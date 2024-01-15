cab37a81caf4060286dbfbb9dcd5fb51cd57f8fe
dp <260348993@qq.com>
2024-01-16 00:08:01 +0800

    leetCode

---
+++ src/main/java/org/example/leetcode/Class88.java
@@ -0,0 +1,22 @@
+package org.example.leetcode;
+
+public class Class88 {
+
+    public static void main(String[] args) {
+        Class88 class88 = new Class88();
+        int[] nums1 = {4,0,0,0,0,0};
+        int[] nums2 = {1,2,3,5,6};
+        class88.merge(nums1, 1, nums2, 5);
+    }
+
+
+    /**
+     * 输入：nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+     * 输出：[1,2,2,3,5,6]
+     * 解释：需要合并 [1,2,3] 和 [2,5,6] 。
+     * 合并结果是 [1,2,2,3,5,6] ，其中斜体加粗标注的为 nums1 中的元素。
+     */
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        return;
+    }
+}
